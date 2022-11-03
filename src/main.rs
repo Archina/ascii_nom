0d323f0b56b1a4da9af436858caae3ca2e2ac0b4
Archina <archina5thrider@gmail.com>
2022-11-03 13:41:09 +0100

    reduce noice a bit for main

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,26 @@ extern crate strum_macros;
 pub mod ascii;
 pub mod mathml;
 
-use crate::{ascii::{exp::parse_expression, sym::{Symbol, parse_symbol}, sym_greek::{Greek, parse_greek}}, mathml::MathMl};
+use crate::{ascii::exp::parse_expression, mathml::MathMl};
 
 fn main() {
-    println!("{:?}", parse_expression("(123)"));
-    println!("{:?}", parse_expression("(3,4,50,x)"));
-    println!("{:?}", parse_expression("[(3,4),(3,3)]"));
-    println!("{:?}", parse_expression("text(Hey)"));
-    println!("{:?}", parse_expression("frac(50)(22:)"));
-    println!("{:?}", parse_expression("root(50){y}"));
-    println!("{:?}", parse_expression("(int) in A"));
-    println!("{:?}", parse_expression("[int:}"));
-    println!("{:?}", parse_expression("theta12huh13Theta"));
+    // println!("{:?}", parse_expression("(123)"));
+    // println!("{:?}", parse_expression("(3,4,50,x)"));
+    // println!("{:?}", parse_expression("[(3,4),(3,3)]"));
+    // println!("{:?}", parse_expression("text(Hey)"));
+    // println!("{:?}", parse_expression("frac(50)(22:)"));
+    // println!("{:?}", parse_expression("root(50){y}"));
+    // println!("{:?}", parse_expression("(int) in A"));
+    // println!("{:?}", parse_expression("[int:}"));
+    // println!("{:?}", parse_expression("theta12huh13Theta"));
     println!("{:?}", parse_expression("thetaThetavarthetaetagamma"));
     println!("{:?}", parse_expression("alphagammaGamma/d"));
 
-    if let Ok((_, exp)) = parse_expression("(alpha)gammaGamma/d"){
+    if let Ok((_, exp)) = parse_expression("alphagammaGamma/d"){
         println!("{}", exp.to_math_ml());
     }
 
-    if let Ok((_, exp)) = parse_symbol("alpha"){
+    if let Ok((_, exp)) = parse_expression("alpha"){
         println!("{}", exp.to_math_ml());
     }
 }
